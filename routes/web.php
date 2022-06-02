ae23114870448ec84fb1685cb982bb522a70419a
evribsap <evribsaputra@gmail.com>
2022-06-02 22:21:23 +0700

    updates home

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [HomeController::class, 'index'])
-        ->name('dashboard');
+        ->name('home');
 
 Route::prefix('admin')
     //->namespace('admin')
