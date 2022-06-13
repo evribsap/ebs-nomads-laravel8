03d78df083fe6fb246886324b923e4cc83330472
evribsap <evribsaputra@gmail.com>
2022-06-13 20:56:14 +0700

    make database travel

---
+++ database/migrations/2022_06_13_134617_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use phpDocumentor\Reflection\Types\Nullable;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('travel_package_id');
+            $table->integer('user_id')->Nullable();
+            $table->integer('additional_visa');
+            $table->integer('transaction_total');
+            $table->string('transaction_status');
+            // IN_CART, PENDING, SUCCESS, CANCEL, FAILED
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
